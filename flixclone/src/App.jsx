0df2ce42d1b6f4c131438ed8da9f8d2c3b474ab2
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-18 08:41:31 +0545

    banner done

--- flixclone/src/App.jsx
+++ flixclone/src/App.jsx
@@ -9,7 +9,7 @@ import requests from './requests'
 
 const App = () => {
   return (
-    <div>
+    <div className="app"> 
       {/* navbar */}
 
 
@@ -18,10 +18,11 @@ const App = () => {
       <Banner />
 
       {/* diffcategories rows */}
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTrending}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
       <Row title="Documentries" fetchUrl={requests.fetchDocumentariesMovies}/>
--- flixclone/src/Banner.jsx
+++ flixclone/src/Banner.jsx
@@ -38,12 +38,17 @@ const Banner = () => {
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List</button>
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
 
       </div>
+
+      <div className="banner--fadeBottom">
+
+      </div>
+
     </header>
   );
 };
